46e306049d9f3974ae32e7cb516a3da6660e1e94
Suren-devops <prithivisuren77@gmail.com>
2026-06-10 22:23:57 +0530

    changed the s3 file

--- aws-S3/backend-s3/main.tf
+++ aws-S3/backend-s3/main.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-terraform-state-bucket"
+resource "aws_s3_bucket" "demo_bucket" {
+
+  bucket = "suren-demo-bucket-123456"
+  tags = {
+    Name = "Demo Bucket"
+    Env  = "Dev"
+  }
 }
